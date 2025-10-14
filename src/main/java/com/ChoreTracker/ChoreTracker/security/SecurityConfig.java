248881dab67a127207b99738db725bc68216b04c
adNord <adno96@gmail.com>
2025-10-14 14:45:45 +0200

    Uppdaterat API path till auth endpointsen

--- src/main/java/com/ChoreTracker/ChoreTracker/security/SecurityConfig.java
+++ src/main/java/com/ChoreTracker/ChoreTracker/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login", "/api/auth/register", "/api/auth/refresh-token")
+                        .requestMatchers("/api/auth/login", "/api/auth/register")
                         .permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
