686ed0ad678b250679041fdc7544a1b1c2894853
adNord <adno96@gmail.com>
2025-10-31 07:11:00 +0100

    Lagt till frontenddeploy till allowed origins

--- src/main/java/com/ChoreTracker/ChoreTracker/security/SecurityConfig.java
+++ src/main/java/com/ChoreTracker/ChoreTracker/security/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:4200",
-                "http://localhost:5173"));
+                "http://localhost:5173",
+                "https://sidequest-choretracker-frontend.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
